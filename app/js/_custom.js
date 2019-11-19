9c54c560a83c755ec0972fc81ab01da79ccfe1f4
alex27-ru <medved.lite@gmail.com>
2019-11-19 21:46:28 +0700

    Add jquery, script burger and header__burger

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -1,5 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
 
 	// Custom JS
+	$('.burger,.header__burger').click(function(event) {
+		$('.burger,.header__burger').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
 
 });
