e33f214d90a18741d85d11e9ee23b1ea2af3fa52
alex27-ru <medved.lite@gmail.com>
2019-11-23 15:27:27 +0700

    Add burger and mobile navigation

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -1,5 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
 
 	// Custom JS
-
+	$('.burger,.header-nav').click(function(event) {
+		$('.burger,.header-nav').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
 });
