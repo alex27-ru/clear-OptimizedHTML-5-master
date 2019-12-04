52d902739871d07c443269771f36c856461b01a8
alex27-ru <medved.lite@gmail.com>
2019-12-04 18:20:44 +0700

    Responsive menu, burger, jq.

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -1,5 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
 
 	// Custom JS
+	$('.burger,.header-nav').click(function(event) {
+	$('.burger,.header-nav').toggleClass('active');
+	$('body').toggleClass('lock');
+	});
 
 });
