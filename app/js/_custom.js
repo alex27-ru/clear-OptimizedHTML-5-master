0def20e36943d7ae0cec8c15bcbaca0de05828bc
alex27-ru <medved.lite@gmail.com>
2021-10-06 17:26:42 +0700

    Add jQ on click and close call-back and fast-request

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -1,11 +1,31 @@
 $(function () {
 
-$('.call__btn').on(click, function () {
-	$('.call-back').addClass('modal-active');
+// $('.call__btn').on('click', function () {
 
+// 	$('.modal').addClass('modal-active-flex');
+// 	$('.call-back').addClass('modal-active');
+
+// });
+
+
+$('.modal__close').on('click', function () {
+	$(this).parent().removeClass('modal-active');
+	$('.modal').removeClass('modal-active-flex');
+});
+$('.modal').on('click', function () {
+	$('.modal').removeClass('modal-active-flex');
+	$('.thanks').removeClass('modal-active');
+	$('.call-back').removeClass('modal-active');
+	$('.fast-request').removeClass('modal-active');
 });
 
 
+$('[data-click]').on('click', function () {
+	var cl = $(this).attr('data-click');
+	$('.modal').addClass('modal-active-flex');
+	$('.' + cl).addClass('modal-active');
+
+});
 
 
 });
