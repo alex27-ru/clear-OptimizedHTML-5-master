b78f2449f95e0450cff3c5abe3e72c3ac0a40caa
alex27-ru <medved.lite@gmail.com>
2021-10-27 21:16:49 +0700

    Add section warranty and plugin magnific-popup

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,32 @@
 // // Import vendor jQuery plugin example
 // import '~/app/libs/mmenu/dist/mmenu.js'
+import '~/app/libs/magnific-popup/dist/jquery.magnific-popup.min.js'
 
 document.addEventListener('DOMContentLoaded', () => {
 
 	// Custom JS
+
+	$('#warranty1-click').magnificPopup({
+		items: {
+      src: '#warranty1',
+      type: 'inline'
+  }
+	});
+	$('#warranty2-click').magnificPopup({
+		items: {
+      src: '#warranty2',
+      type: 'inline'
+  }
+	});
+	$('#warranty3-click').magnificPopup({
+		items: {
+      src: '#warranty3',
+      type: 'inline'
+  }
+	});
+
+
+
+
 
 })
